6f0326c5a533910780a39ee7ebd7cecd1c795e43
rajeevmandover92 <149289676+rajeevmandover92@users.noreply.github.com>
2026-05-30 14:02:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector('button');
+let p = document.querySelector('#result');
+
+
+btn.addEventListener('click', async() => {
+    let fact = await getFacts();
+    p.innerText = fact;
+})
+
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts(){
+    try{
+        let res = await axios.get(url);
+        return res.data.fact;
+    }
+    catch(e){
+        return "NO FACT FOUND";
+    }
+}
+
